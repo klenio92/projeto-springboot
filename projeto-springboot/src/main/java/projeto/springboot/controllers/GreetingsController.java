fb6afd75c84054e3780adac234c6b273e865c947
User <User@DESKTOP-5EUGTCV>
2023-10-25 13:26:14 -0300

    Criando primeiro metodo listar - Criando CRUD

--- projeto-springboot/src/main/java/projeto/springboot/controllers/GreetingsController.java
+++ projeto-springboot/src/main/java/projeto/springboot/controllers/GreetingsController.java
@@ -1,11 +1,20 @@
 package projeto.springboot.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import projeto.springboot.model.Usuario;
+import projeto.springboot.repository.UsuarioRepository;
 
 /**
  *
@@ -13,14 +22,34 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GreetingsController {
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	/**
+	 *
+	 * @param name the name to greet
+	 * @return greeting text
+	 */
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String greetingText(@PathVariable String name) {
+
+		Usuario usuario = new Usuario();
+		usuario.setNome(name);
+		usuarioRepository.save(usuario);
+
+		return "Hello " + name + "!";
+	}
+
+	@GetMapping(value = "listatodos")
+	@ResponseBody
+	public ResponseEntity<List<Usuario>> listaUsuario() {
+
+		List<Usuario> usuarios = usuarioRepository.findAll();
+
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+
+	}
+
 }
---
+++ projeto-springboot/src/main/java/projeto/springboot/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package projeto.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import projeto.springboot.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
